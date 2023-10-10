b0fef31e7cdeca08336e6b1c2cf5c610f3af6861
Tekk0n <bru.novaruiz@gmail.com>
2023-10-10 18:56:09 +0200

    Relation between prov and ccaa selects works

--- xlsxReader.js
+++ xlsxReader.js
@@ -1,5 +1,6 @@
 document.getElementById('xlsxFileInput').addEventListener('change', function (event) {
-    var file = event.target.files[0]
+    const file = event.target.files[0]
+
     const reader = new FileReader()
     reader.onload = function (event) {
         // Este recorrido puede simplificarse pero de esta manera
@@ -10,34 +11,53 @@ document.getElementById('xlsxFileInput').addEventListener('change', function (ev
         console.log(data)
 
         var desiredsheet = data.Sheets[data.SheetNames[0]]
-        console.log('Array CRUDOde toda la pagina xlsx:')
+        var desiredSheet2 = data.Sheets[data.SheetNames[1]]
+        console.log('Array CRUDO de toda la pagina xlsx:')
         console.log(desiredsheet)
+        console.log(desiredSheet2)
 
-        var jsondata = XLSX.utils.sheet_to_json(desiredsheet)
+        const jsondata = XLSX.utils.sheet_to_json(desiredsheet)
+        const jsondata2 = XLSX.utils.sheet_to_json(desiredSheet2)
         console.log('Array JSON de toda la pagina xlxs:')
         console.log(jsondata)
+        console.log(jsondata2)
 
-        var jsonArray = jsondata.filter(function (array) {
-            return array.id
+        // Lo suyo filtrar antes de rellenar el html, 
+        // pero en este caso no hace falta
+        rellenarSelect(jsondata, 'selectCcaa')
+
+        // Esta llamada la hacemos desde aqui para poder manejar los valores de jsondata2
+        document.getElementById('selectCcaa').addEventListener('change', function (event) {
+            var ccaaSeleccionada = event.target.value
+            var provinciasFiltradas = filtrarProvincias(ccaaSeleccionada, jsondata2)
+            rellenarSelect(provinciasFiltradas, 'selectProvincia')
         })
-        console.log(jsonArray)
-
-        rellenarSelect(jsonArray)
     };
     // Si no leemos el buffer binario el reader no lo 
     // procesa y no se obtiene nada
     reader.readAsArrayBuffer(file)
 })
 
-function rellenarSelect(array) {
-    var select = document.getElementById('selectProvincia')
 
-    for (var object of array){
+
+function rellenarSelect(array, nombreDelSelect) {
+    var select = document.getElementById(nombreDelSelect)
+    // Limpia el select antes de introducir los elementos nuevos
+    select.innerHTML = ''
+
+    for (var object of array) {
         var option = document.createElement('option')
         // Es aqui donde la logica cambia segun los 
         // datos del xlsx
-        option.value = object.id
-        option.text = object.nombre
+        option.value = object.Id
+        option.text = object.Descripcion
         select.appendChild(option)
     }
+}
+
+function filtrarProvincias(ccaaId, arrayProvincias) {
+    var provinciasFiltradas = arrayProvincias.filter(function(provincia){
+        return provincia.Comunidadautonoma == ccaaId
+    })
+    return provinciasFiltradas
 }
